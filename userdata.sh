1d96fb7a0052c92a082b078a29c1c5e6a9a45b22
vedacodes <99849936+vedacodes@users.noreply.github.com>
2022-04-05 19:05:32 -0500

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -8,4 +8,4 @@ chown -R ec2-user:apache /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
-echo "Hello" > /var/www/html/index.html
+echo "Hello from Veda" > /var/www/html/index.html
